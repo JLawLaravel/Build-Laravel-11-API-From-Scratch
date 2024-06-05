943b2f69327750e15a720f36ade1c2dfd1c7a8ae
LawrenceWebon <lawrencewebon@gmail.com>
2024-06-05 20:18:17 +0800

    Setup vue and and add pagination using api

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp } from "vue";
+import PostsIndex from "./components/Posts/Index.vue";
+import { Bootstrap4Pagination } from "laravel-vue-pagination";
+import { Bootstrap5Pagination } from "laravel-vue-pagination";
+import { TailwindPagination } from "laravel-vue-pagination";
+
+createApp({}).component("PostsIndex", PostsIndex).mount("#app");
